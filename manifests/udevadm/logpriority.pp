4a0a3d367990176f69bcbd3f95b147a77113687e
janfickler <65912137+janfickler@users.noreply.github.com>
2026-02-18 14:09:30 +0100

    Comment out log priority validation
    Commented out the validation for udev_log parameter.

--- manifests/udevadm/logpriority.pp
+++ manifests/udevadm/logpriority.pp
@@ -21,7 +21,7 @@
 class udev::udevadm::logpriority(
   $udev_log = 'err'
 ) inherits udev::params {
-  validate_re($udev_log, '^err$|^info$|^debug$')
+  #validate_re($udev_log, '^err$|^info$|^debug$')
 
   exec { "${udev::params::udevlogpriority}=${udev_log}":
     refreshonly => true,
