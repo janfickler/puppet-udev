8f4a0782f1a164aff59fad7fd15250909af72de4
janfickler <65912137+janfickler@users.noreply.github.com>
2026-02-18 14:07:20 +0100

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -24,8 +24,8 @@ class udev(
   $config_file_replace = $udev::params::config_file_replace,
   $rules = $udev::params::rules,
 ) inherits udev::params {
-  validate_re($udev_log, '^err$|^info$|^debug$')
-  validate_bool($config_file_replace)
+  #validate_re($udev_log, '^err$|^info$|^debug$')
+  #validate_bool($config_file_replace)
 
   anchor { 'udev:begin': }
   -> package{ $udev::params::udev_package:
